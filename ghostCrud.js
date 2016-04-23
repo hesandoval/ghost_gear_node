97d0bf155d90fb9870b97c887b062ffa4464d2c6
Hector Sandoval <hesandova@gmail.com>
2016-04-23 11:14:13 -0700

    Added some stuff

--- ghostCrud.js
+++ ghostCrud.js
@@ -1,8 +1,12 @@
+var mongoose = require("mongoose");
+var ghostSchema = require("./api_specifications/models/ghost_json_api.js");
 function setup(io){
 
     io.on("connection", function(socket) {
         console.log("A user has connected to the page");
-        //socket.emti("")
+        socket.on("get_point_cloud", function(callback){
+            ghostSchema.find({}, "location", callback);
+        });
     });
 }
 
--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -2,3 +2,10 @@
  * Created by edsan on 4/23/16.
  */
 var socket = io();
+socket.emit('get_point_cloud', function(err, data){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(data);
+    }
+});
