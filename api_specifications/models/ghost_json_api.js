3326541912e32739db6b5cb7c2f732fa0265de4f
Alcides Sorto <alsorto@gmail.com>
2016-04-23 00:53:45 -0700

    added ghost equipment schema

---
+++ api_specifications/models/ghost_json_api.js
@@ -0,0 +1,41 @@
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ghostSchema = new Schema({
+            source: {
+                first_name: String,
+                last_name: String,
+                email: String,
+                contact_number: Number,
+                role: String,
+            },
+            location: {
+                detail: String,
+                location: {
+                    lat: Number,
+                    lng: Number
+                }
+            },
+            net_data: {
+                color: String,
+                mesh_size: Number,
+                twine_size: Number,
+                net_code: String,
+                estimate_net_width: Number,
+                estimate_net_length: Number,
+                comments: String,
+            },
+            wildlife_data: {
+                animals: Boolean,
+                animal_descripton: [{
+                    code: Number,
+                    condition: String,
+                    length_size: Number
+                }]
+            }
+        }
+
+var ghostSchema = mongoose.model('ghostSchema', ghostSchema);
+
+export.models = ghostSchema;
--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var hogan = require('hogan-express');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+
 var app = express();
 
 // view engine setup
