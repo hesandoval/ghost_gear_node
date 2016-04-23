9ed564938a343c8728a16bbac9a4eb0ed9da645d
Hector Sandoval <hesandova@gmail.com>
2016-04-23 06:33:13 -0700

    Added geolocation

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,10 +1,29 @@
 /**
  * Created by edsan on 4/23/16.
  */
+
 function initMap() {
+    var initialLocation = new google.maps.LatLng(36.617906, -121.901943);
+    var browserSupportFlag = new Boolean();
     var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 36.617906, lng: -121.901943},
         zoom: 17
     });
+
+    // Try W3C Geolocation (Preferred)
+    if(navigator.geolocation) {
+        browserSupportFlag = true;
+        navigator.geolocation.getCurrentPosition(function(position) {
+            initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+            map.setCenter(initialLocation);
+        }, function() {
+            handleNoGeolocation(browserSupportFlag);
+        });
+    }
+    // Browser doesn't support Geolocation
+    else {
+        browserSupportFlag = false;
+        map.setCenter(initialLocation);
+    }
     window.map = map;
+
 }
